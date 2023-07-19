8efb7ac9c343e10291775f53bf6f23c19e99451b
arc-yasha <helminaluri97@gmail.com>
2023-07-20 00:10:02 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+  {
+    id: 'sadfasdf',
+    title: 'sdafasdf',
+    createdAt: 'fasdf',
+    updatedAt: 'striasdfasdfng',
+    tags: ['asdfas', 'asdf'],
+    body: 'sadfasdf',
+  },
+];
+
+module.exports = notes;
